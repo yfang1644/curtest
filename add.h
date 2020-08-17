13d29df9771a6b1a66374f6d208628ca34bc6de7
yfang1644 <yfang1644@gmail.com>
2020-08-17 19:55:28 +0800

    first commit

---
+++ add.h
@@ -0,0 +1,6 @@
+#ifndef _ADD_H
+#define _ADD_H
+
+float add(float a, float b);
+
+#endif
---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "add.h"
+
+int main(int argc, char *argv[])
+{
+    float x, y, z;
+
+    if(argc == 3) {
+        y = atof(argv[1]);
+        z = atof(argv[2]);
+
+        x = add(y, z);
+        printf("%f\n", x);
+    } else {
+        printf("command line invalid\n");
+        return 1;
+    }
+	return 0;
+}
